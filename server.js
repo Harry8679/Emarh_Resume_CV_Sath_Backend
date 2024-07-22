d0b64ab5d0dcd8f01cab1ee01b046a55c337b9b2
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-23 00:42:11 +0200

    Connect to Mongo Database

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const connectDB = require('./config/connectDB');
 
 dotenv.config();
 
 const port = process.env.PORT || 4000;
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
