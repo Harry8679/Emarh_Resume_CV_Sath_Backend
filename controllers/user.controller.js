dd1caf1ed623bec2eb8cba69160de9e2669817ad
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-25 12:46:12 +0200

    Login Controller : check username and password

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,59 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/user.model');
 
-const login = asyncHandler(async(req, res) => {
-    res.send('Login Page API');
+/** ------------------- Login -------------------- */
+const login = asyncHandler(async(req, res, next) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return next(new ErrorHandler('Please enter username and password', 400));
+    }
+
+    // Find user in the database
+    const user = await User.findOne({ username }).select('+password');
+
+    if (!user) {
+        return next(new ErrorHandler('Invalid username or password', 401));
+    }
+
 });
 
 module.exports = { login };
+
+// /*----------------- Register an Account ----------------- */
+// export const register = asyncHandler(async(req, res) => {
+//     const { name, email, password } = req.body;
+
+    
+//     const user = await User.create({ name, email, password });
+
+//     const token = user.getJwtToken();
+
+//     // res.status(201).json({ token });
+//     sendToken(user, 201, res);
+// });
+
+// /*----------------- Login ----------------- */
+// export const loginUser = asyncHandler(async(req, res, next) => {
+//     const { email, password } = req.body;
+
+//     if (!email || !password) {
+//         return next(new ErrorHandler('Please enter email and password', 400));
+//     }
+
+//     // Find user in the database
+//     const user = await User.findOne({ email }).select('+password');
+
+//     if (!user) {
+//         return next(new ErrorHandler('Invalid email or password', 401));
+//     }
+
+//     // Check if password is correct
+//     const isPasswordMatched = await user.comparePassword(password);
+
+//     if (!isPasswordMatched) {
+//         return next(new ErrorHandler('Invalid or password', 401));
+//     }
+
+//     sendToken(user, 200, res);
+// });
