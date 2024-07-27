dcdb4f4838858ff6fa6a1f0a086226330d8b2540
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-27 09:52:52 +0200

    User controller updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -56,45 +56,6 @@ const register = asyncHandler(async(req, res) => {
         if (err) throw err;
         res.json({ token });
     });
-    // res.send('User created successfully');
 });
 
 module.exports = { login, register };
-
-// /*----------------- Register an Account ----------------- */
-// export const register = asyncHandler(async(req, res) => {
-//     const { name, email, password } = req.body;
-
-    
-//     const user = await User.create({ name, email, password });
-
-//     const token = user.getJwtToken();
-
-//     // res.status(201).json({ token });
-//     sendToken(user, 201, res);
-// });
-
-// /*----------------- Login ----------------- */
-// export const loginUser = asyncHandler(async(req, res, next) => {
-//     const { email, password } = req.body;
-
-//     if (!email || !password) {
-//         return next(new ErrorHandler('Please enter email and password', 400));
-//     }
-
-//     // Find user in the database
-//     const user = await User.findOne({ email }).select('+password');
-
-//     if (!user) {
-//         return next(new ErrorHandler('Invalid email or password', 401));
-//     }
-
-//     // Check if password is correct
-//     const isPasswordMatched = await user.comparePassword(password);
-
-//     if (!isPasswordMatched) {
-//         return next(new ErrorHandler('Invalid or password', 401));
-//     }
-
-//     sendToken(user, 200, res);
-// });
